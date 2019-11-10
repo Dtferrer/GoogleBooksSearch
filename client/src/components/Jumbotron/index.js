fb0851891253b058316f79228cd71b4973185d72
Danica Ferrer <dtferrer@ucsd.edu>
2019-11-09 23:24:21 -0800

    Updated jumbotron to not link to other pages

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -5,7 +5,7 @@ function Jumbotron() {
     <div className="jumbotron text-center">
       <h1><span><a className="white" id="jumboHead" href="/">(React) Google Books Search</a></span></h1>
         <div>
-          <a className="white" target="_blank" rel="noopener noreferrer" href="http://developers.google.com/books/">
+          <a className="white" target="_blank" rel="noopener noreferrer">
             Search for and Save Books of Interest
           </a>
         </div>
