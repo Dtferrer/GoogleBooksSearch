b3f5bba842dbed91615d6e1b6be93e07a1ccdc4c
Danica Ferrer <dtferrer@ucsd.edu>
2019-11-09 23:18:48 -0800

    Updated Jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -3,10 +3,10 @@ import React from "react";
 function Jumbotron() {
   return (
     <div className="jumbotron text-center">
-      <h1><span><a className="white" id="jumboHead" href="/">My Virtual Bookshelf</a></span></h1>
+      <h1><span><a className="white" id="jumboHead" href="/">(React) Google Books Search</a></span></h1>
         <div>
           <a className="white" target="_blank" rel="noopener noreferrer" href="http://developers.google.com/books/">
-            Powered by Google Books
+            Search for and Save Books of Interest
           </a>
         </div>
       
